442e5c715471f24a2ccce6d769792ef7d7fd7a62
SebasGTX1 <sebas-0202@hotmail.es>
2023-10-25 16:18:43 -0500

    Debug TRUE

--- empresarialweb/empresarialweb/settings.py
+++ empresarialweb/empresarialweb/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-!8r4tyg*@-j-tos#w(epfsw0rsbc_&*qotslzog-+jzcy6plyn'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
